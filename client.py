f95b22390f4b0af7e2e9187be570a4efce4dba32
shree <singhishree@gmail.com>
2023-02-23 02:39:01 +0530

    completes the client-server connection and streams art-ified image
    Signed-off-by: shree <singhishree@gmail.com>

--- client.py
+++ client.py
@@ -6,31 +6,24 @@ Created on Mon Feb 20 14:14:07 2023
 @author: shree
 """
 
-import socket
+from socket import socket, AF_INET, SOCK_STREAM
 import cv2
 import pickle
 import struct
+import converter
+import hasher
 
-
-client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-host_ip = '127.0.1.1'
-PORT = 11027
-client.connect((host_ip, PORT)) 
-
-data = b""  # creates an empty data packet
-
-HEADERSIZE = struct.calcsize("Q")
-
-while True:
+def retrieve():
+    global data, HEADER_SIZE
     # first we receive the header (size of the pickle file)
-    while len(data) < HEADERSIZE:
+    while len(data) < HEADER_SIZE:
         # the smaller the size of datapacket, the longer it takes to receive
         packet = client.recv(2**12)
         if not packet: break
         data += packet
     
-    msg_size = struct.unpack("Q", data[:HEADERSIZE])[0]
-    data = data[HEADERSIZE:]
+    msg_size = struct.unpack("Q", data[:HEADER_SIZE])[0]
+    data = data[HEADER_SIZE:]
     
     while len(data) < msg_size:
         data += client.recv(2**12)
@@ -38,9 +31,32 @@ while True:
     pickle_data = data[:msg_size]
     data  = data[msg_size:] # the remainder of data received is stored for the next loop 
     
-    frame = pickle.loads(pickle_data)
-    cv2.imshow('client', frame)
-    cv2.waitKey(10) 
+    return pickle.loads(pickle_data)
 
-    
+############# SERVER STUFF ##################
+
+client = socket(AF_INET, SOCK_STREAM)
+host_ip = '192.168.29.126'
+PORT = 11021
+client.connect((host_ip, PORT)) 
+
+data = b""  # creates an empty data packet
+
+HEADER_SIZE = struct.calcsize("Q")
+
+############# VIDEO STUFF #####################
+
+im_list = retrieve()
+bgr_avg, hashbin = hasher.main(im_list)
+
+image = retrieve()
+converter.init(image.shape[0], image.shape[1], im_list, bgr_avg, hashbin, w_size=1)
+
+while True:
+    image = retrieve()
+    converter.convert(image)
+
+    cv2.imshow('client', converter.result_image)
+    cv2.waitKey(10)
+
 client.close()
--- server.py
+++ server.py
@@ -7,36 +7,85 @@ Created on Mon Feb 20 12:57:44 2023
 """
 import struct
 import cv2
-import socket
+from socket import socket, AF_INET, SOCK_STREAM
 import pickle
+import hasher
+import converter
+import psutil
 
-server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+def get_ips():
+    ips = dict()
+    for interface, snics in psutil.net_if_addrs().items():
+        for snic in snics:
+            if snic.family == AF_INET and interface:
+                ips[interface] = snic.address
+    return ips
 
-host_ip = socket.gethostbyname(socket.gethostname())
-PORT = 11027
+def send(array):
+    pickle_data = pickle.dumps(array)
+    
+    # size of pickle is stored in a long long int (represented by Q)
+    message = struct.pack("Q", len(pickle_data)) + pickle_data
+    client.sendall(message)
+
+############# SERVER STUFF ##################
+
+ips = get_ips()
+
+server = socket(AF_INET, SOCK_STREAM)
+
+for key in ips.keys():
+    if key.startswith('wlp'):
+        host_ip = ips[key]
+        break
+else:
+    host_ip = '127.0.1.1'
+
+PORT = 11021
 print('HOST IP and PORT:', host_ip, PORT)
 
-socket_address = (host_ip,PORT)
+socket_address = (host_ip, PORT)
 server.bind(socket_address)
 server.listen(5)
 
 print('Listening')
 
-try:
-    while True:
-        client, addr = server.accept()
-        print('Connection from:',addr)
-        if client:
-            video = cv2.VideoCapture(0)
-            while(video.isOpened()):
-                showing, frame = video.read()
-                pickle_data = pickle.dumps(frame)
+client, addr = server.accept()
+print(f'Connected to: {addr}')
+
+############# VIDEO STUFF #####################
+
+window_size = 20
+tile_size = 20
+
+im_list = hasher.create_imarray(tile_size, 'subimages')
+send(im_list)
+bgr_avg, hashbin = hasher.main(im_list)
+
+video = cv2.VideoCapture(0)
+showing, image = video.read()
+
+converter.init(image.shape[0]//window_size, image.shape[1]//window_size,
+               im_list, bgr_avg, hashbin, window_size)
+
+
+try:    
+    # if client:
+                    
+        while(video.isOpened()):
+            
+            image = video.read()[1]
+            small_image = converter.convert(image)    
+            
+            send(small_image)
+
+            cv2.imshow('original', small_image)
+            cv2.imshow('new', converter.result_image)
+            
+            cv2.waitKey(10)   
                 
-                # size of pickle is stored in a long long int (represented by Q)
-                message = struct.pack("Q", len(pickle_data)) + pickle_data
-                client.sendall(message)
-                cv2.imshow('server',frame)
-                cv2.waitKey(10)
+
+                
 except KeyboardInterrupt:
     client.close()
     video.release()
