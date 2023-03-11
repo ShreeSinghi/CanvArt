e19fb42c9a62d0480bb65b86922fe7e512949c01
shree <singhishree@gmail.com>
2023-03-11 12:14:06 +0530

    adds file converter backend

--- client.py
+++ client.py
@@ -6,12 +6,23 @@ Created on Mon Feb 20 14:14:07 2023
 @author: shree
 """
 
-from socket import socket, AF_INET, SOCK_STREAM
+from socket import socket, AF_INET, SOCK_STREAM, gaierror
 import cv2
 import pickle
 import struct
+import os
 import converter
 import hasher
+from joblib import dump, load
+
+def save_data(hashbin, im_list, bgr_avg):
+    dump(hashbin, 'hash.joblib')
+    dump(im_list, 'imarray.joblib')
+    dump(im_list, 'bgravg.joblib')
+
+def load_data():
+    return load('hash.joblib'), load('imarray.joblib'), load('bgravg.joblib')
+
 
 def retrieve():
     global data, HEADER_SIZE
@@ -35,30 +46,46 @@ def retrieve():
     
     return pickle.loads(pickle_data)
 
+def feed_generator(host_ip, port, glob_vars):
+    global data, HEADER_SIZE, client
+    while True:
+        try:
 ############# SERVER STUFF ##################
-
-client = socket(AF_INET, SOCK_STREAM)
-HOST_IP = input("IP: ")
-PORT = int(input("Port: "))
-client.connect((HOST_IP, PORT)) 
-
-data = b""  # creates an empty data packet
-
-HEADER_SIZE = struct.calcsize("Q")
-
+            
+            glob_vars["message"] = "Connecting..."
+        
+            client = socket(AF_INET, SOCK_STREAM)
+            client.connect((host_ip, port)) 
+            
+            data = b""  # creates an empty data packet
+            
+            HEADER_SIZE = struct.calcsize("Q")
+            
+            glob_vars["message"] = "Connected!"
+            
 ############# VIDEO STUFF #####################
-
-im_list = retrieve()
-bgr_avg, hashbin = hasher.main(im_list)
-
-image = retrieve()
-converter.init(image.shape[0], image.shape[1], im_list, bgr_avg, hashbin, w_size=1)
-
-while True:
-    image = retrieve()
-    converter.convert(image)
-
-    cv2.imshow('client', converter.result_image)
-    cv2.waitKey(10)
-
-client.close()
+            
+        
+            tile_size = 20
+            
+            if all(x in os.listdir() for x in ['hash.joblib', 'imarray.joblib', 'bgravg.joblib']):
+                hashbin, im_list, bgr_avg = load_data()
+            else:
+                im_list = hasher.create_imarray(tile_size, 'subimages')
+                bgr_avg, hashbin = hasher.main(im_list)
+                save_data(hashbin, im_list, bgr_avg)
+            
+            image = retrieve()
+            converter.init(image.shape[0], image.shape[1], im_list, bgr_avg, hashbin, w_size=1)
+            
+            yield b'--frame\r\n'
+        
+            while True:
+                image = retrieve()
+                converter.convert(image)
+            
+                frame = cv2.imencode('.bmp', converter.result_image)[1].tobytes()
+                
+                yield b'Content-Type: image/bmp\r\n\r\n' + frame + b'\r\n--frame\r\n'
+        except gaierror:
+            glob_vars["message"] = "Incorrect details"
--- web.py
+++ web.py
@@ -5,85 +5,72 @@ Created on Tue Feb 21 01:04:38 2023
 
 @author: shree
 """
-from flask import Flask, redirect, render_template, request, Response, session
-from flask_session import Session
+from flask import Flask, render_template, request, Response, session, send_file
 import server
+import client
 import json
 import sys
-from threading import Thread
-from socket import socket, AF_INET, SOCK_STREAM
-import psutil
-
-def get_ips():
-    ips = dict()
-    for interface, snics in psutil.net_if_addrs().items():
-        for snic in snics:
-            if snic.family == AF_INET and interface:
-                ips[interface] = snic.address
-    return ips
 
 app = Flask(__name__)
-app.config.from_object(__name__)
-Session(app)
-
-def server_connect():
-    global client_socket, server_socket, HOST_IP, PORT, connected
-    
-    ips = get_ips()
-    
-    server_socket = socket(AF_INET, SOCK_STREAM)
-    
-    for key in ips.keys():
-        if key.startswith('wlp'):
-            HOST_IP = ips[key]
-            break
-    else:
-        HOST_IP = '127.1.1.1'
-    server_socket.bind((HOST_IP, 0))
-
-    PORT = server_socket.getsockname()[1]
-
-    with open('data.txt', 'w') as f:
-        f.write(f"{HOST_IP} {PORT}")
-
-    server_socket.listen(4)
-    
-    print('Listening', file=sys.stderr)
-    
-    client_socket, addr = server_socket.accept()
-    print(f'Connected to: {addr}', file=sys.stderr)
-    connected = True
-
+app.secret_key = "WoC"
+glob_vars = dict()
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route('/video_feed')
-def video_feed():
+@app.route("/download")
+def getPlotCSV():
+
+    csv = '1,2,3\n4,5,6\n'
+    return Response(
+                    csv,
+                    mimetype="text/csv",
+                    headers={"Content-disposition":
+                             "attachment; filename=myplot.csv"}
+    )
+
+
+@app.route('/feed_server')
+def video_server():
     global client_socket
+
     return Response(server.feed_generator(client_socket),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route('/status', methods=['GET'])
-def getStatus():
-  status_list = {'HOST_IP': HOST_IP, 'PORT': str(PORT), 'connected': str(connected)}
-  return json.dumps(status_list)
+@app.route('/feed_client')
+def video_client():
+    global host_ip, port, glob_vars
+    glob_vars["message"] = ""
+    return Response(client.feed_generator(host_ip, port, glob_vars),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/server_status', methods=['GET'])
+def server_status():  
+  return json.dumps(glob_vars)
+
+@app.route('/client_status', methods=['GET'])
+def client_status():
+  return json.dumps(glob_vars)
 
 
 @app.route("/server")
 def server_page():
-    global HOST_IP, PORT, connected
-    HOST_IP, PORT, connected = "", 0, False
+    global host_ip, port, connected
+    host_ip, port, connected = "", "", "False"
 
-    t = Thread(target=server_connect)
-    t.start()
     return render_template("server.html")
 
 @app.route("/client", methods=["POST", "GET"])
 def client_page():
-    return render_template("client.html")
+    global host_ip, port
+    host_ip, port = "", 0
+    if request.method == "POST":
+
+        host_ip, port = str(request.form['ip']), int(request.form['port'])
+        return render_template("client.html", video_url="/feed_client")
+    return render_template("client.html", video_url="")
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5034)
+    app.run(debug=True, port=5048)
